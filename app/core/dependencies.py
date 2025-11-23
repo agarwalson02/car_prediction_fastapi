40955af9f3f999cc825f24534592034660c6af55
agarwalson02 <agarwalson02@gmail.com>
2025-11-23 22:23:34 +0530

    added authorization

---
+++ app/core/dependencies.py
@@ -0,0 +1,14 @@
+from fastapi import Header, HTTPException
+from app.core.security import verify_token
+from app.core.settings import settings
+
+def get_api_key(api_key:str = Header(...)):
+    if api_key !=settings.API_KEY:
+        raise HTTPException(status_code=401,detail="Invalid API Key")
+
+def get_current_user(token:str = Header(...)):
+    payload=verify_token(token)
+    if not payload:
+        raise HTTPException(status_code=401,detail="Invalid Token")
+    return payload
+    
